3c6b7f8f6da40b7f120950fd8e3e889bdac9826a
Carlos Tão <113472199+813648@users.noreply.github.com>
2026-09-22 11:01:53 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-magico-tata-v10';
+const CACHE_NAME = 'portal-magico-tata-v12';
 const APP_SHELL = ['./', './index.html', './icone.svg', './icone-192.png', './icone-512.png'];
 
 self.addEventListener('install', event => {
